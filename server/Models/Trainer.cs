b844c818262da35e6d657d08fb3e9d4766d4412a
ZYX056 <zxiong056@gmail.com>
2026-01-15 17:32:13 -0600

    Revise comments for Trainer class

--- server/Models/Trainer.cs
+++ server/Models/Trainer.cs
@@ -5,16 +5,11 @@
     /// </summary>
     public class Trainer
     {
-        public int Id { get; set; }
-        // Unique username/login for the trainer
-        public string UserName { get; set; }
-        // Display name for trainer character
-        public string DisplayName { get; set; } = null!;
-        // Authentication reference
-        public string AuthUserId { get; set; } = null!;
-        // Navigation: Parties owned by the trainer
-        public ICollection<Party> Parties { get; set; } = new List<Party>();
-        // Navigation: Permissions assigned to this trainer in the campaigns
-        public ICollection<TrainerPermission> Permissions { get; set; } = new List<TrainerPermission>();
+        public int Id { get; set; } // Primary Key
+        public string UserName { get; set; } // Unique username/login for the trainer
+        public string DisplayName { get; set; } = null!; // Display name for trainer character
+        public string AuthUserId { get; set; } = null!; // Authentication reference
+        public ICollection<Party> Parties { get; set; } = new List<Party>(); // Navigation: Parties owned by the trainer
+        public ICollection<TrainerPermission> Permissions { get; set; } = new List<TrainerPermission>(); // Navigation: Permissions assigned to this trainer in the campaigns
     }
 }
